9b4ac6789142fd30c369568626534ad1d0fb239a
katerina-codes <katerinaldg@gmail.com>
2017-03-01 10:17:43 +0000

    Updated exercise 5

--- ex5.rb
+++ ex5.rb
@@ -8,4 +8,9 @@ ans2 = number1 - number2
 ans3 = number1 * number2
 ans4 = number1 / number2
 
-puts "#{number1} \+ #{number2} = #{ans1} \n#{number1} \- #{number2} = #{ans2} \n#{number1} \* #{number2} = #{ans3}\n#{number1} \/ #{number2} = #{ans4}"
+puts """
+  #{number1} \+ #{number2} = #{ans1}
+  #{number1} \- #{number2} = #{ans2}
+  #{number1} \* #{number2} = #{ans3}
+  #{number1} \/ #{number2} = #{ans4}
+  """
