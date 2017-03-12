9c322fa4fe59658d0cf34414bdb639a21614423b
katerina-codes <katerinaldg@gmail.com>
2017-03-12 16:01:09 +0000

    Updated code and fixed bugs in number_comparison.rb

--- ex22/lib/number_comparison.rb
+++ ex22/lib/number_comparison.rb
@@ -1,35 +1,47 @@
-class NumberComparison
-
 #ask the user for 3 numbers
 #check the numbers are different
 #if not exit the program
 #if the numbers are different display largest one
 
+class NumberComparison
+
   def get_values
+    puts "Please enter three different numbers. "
     puts "Enter the first number: "
     num1 = gets.chomp.to_i
     puts "Enter the second number: "
     num2 = gets.chomp.to_i
-    puts "Enter the third number: "
-    num3 = gets.chomp.to_i
-    [num1, num2, num3]
+
+        puts "Enter the third number: "
+        num3 = gets.chomp.to_i
+
+          [num1, num2, num3]
   end
 
 
   def main_program
     num1, num2, num3 = get_values
-    largest_number = check_numbers_are_different(num1, num2, num3)
-    puts "The largest number is #{largest_number}."
+    check_numbers_are_different(num1, num2, num3)
+
+      if !check_numbers_are_different(num1, num2, num3)
+        largest_number = return_largest_number(num1, num2, num3)
+        puts "The largest number is #{largest_number}."
+      else
+        exit
+      end
   end
 
 
   def check_numbers_are_different(num1, num2, num3)
-      if num1 == num2 || num2 == num3 || num1 == num3
-        return
-      else
-      return_largest_number(num1, num2, num3)
-      end
+     num1 == num2 || num2 == num3 || num1 == num3
   end
+
+
+#        exit
+#      else
+#        return_largest_number(num1, num2, num3)
+#      end
+#  end
 
 
   def return_largest_number(num1, num2, num3)
