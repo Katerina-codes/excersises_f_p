0a1ad8ecb2155c40335838769b86fe33a5baf7c0
katerina-codes <katerinaldg@gmail.com>
2017-03-23 13:48:09 +0000

    Added another test to check if two different anagram strings returned true.

--- ex24_Anagram_checker/spec/anagram_checker_spec.rb
+++ ex24_Anagram_checker/spec/anagram_checker_spec.rb
@@ -2,9 +2,14 @@ require 'anagram_checker'
 
 describe AnagramChecker do
 
-  it "should test that words 'aaa' and 'aaa' return true" do
+  it "tests that words 'aaa' and 'aaa' return true" do
     word_checker = AnagramChecker.new
     expect(word_checker.check_if_anagram("aaa", "aaa")).to eq(true)
+  end
+
+  it "tests that 'aba' and 'aba' return true" do
+    word_checker = AnagramChecker.new
+    expect(word_checker.check_if_anagram("baa", "aba")).to eq(true)
   end
 
 end
