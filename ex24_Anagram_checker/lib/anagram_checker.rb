8a5780b9d20b007dcdbcc4fbac010f0fc5247b14
katerina-codes <katerinaldg@gmail.com>
2017-03-23 18:29:51 +0000

    Re-wrote my code to add the constraint of using .length. Tests green.

--- ex24_Anagram_checker/lib/anagram_checker.rb
+++ ex24_Anagram_checker/lib/anagram_checker.rb
@@ -2,23 +2,27 @@ class AnagramChecker
 
   def check_if_anagram(string1, string2)
 
-    if !check_word_length(string1, string2)
-      false
+    if check_string1_length(string1) == check_string2_length(string2)
+      sort_array(string1, string2)
     else
-      string1_array = string1.split("").sort
-      string2_array = string2.split("").sort
+      false
     end
 
-    if string1_array == string2_array
-      true
-    else
-      false
-    end
+    sort_array(string1, string2)
   end
 
-    def check_word_length(string1, string2)
-      string1.length == string2.length
-    end
-  
+  def sort_array(string1, string2)
+    string1_array = string1.split("").sort   # ant
+    string2_array = string2.split("").sort   # ant
+    string1_array == string2_array
+  end
+
+  def check_string1_length(string1)
+    string1.length
+  end
+
+  def check_string2_length(string2)
+    string2.length
+  end
 
 end
--- ex24_Anagram_checker/spec/anagram_checker_spec.rb
+++ ex24_Anagram_checker/spec/anagram_checker_spec.rb
@@ -11,15 +11,21 @@ describe AnagramChecker do
     word_checker = AnagramChecker.new
     expect(word_checker.check_if_anagram("baa", "aba")).to eq(true)
   end
-  
+
   it "tests that 'abc' and 'aba' return false" do
     word_checker = AnagramChecker.new
     expect(word_checker.check_if_anagram("abc", "aba")).to eq(false)
   end
-  #
-  # it "tests that 'tan' and 'ant' return true" do
-  #   word_checker = AnagramChecker.new
-  #   expect(word_checker.check_if_anagram("tan", "ant")).to eq(true)
-  # end
+
+  it "tests that 'tan' and 'ant' return true" do
+    word_checker = AnagramChecker.new
+    expect(word_checker.check_if_anagram("tan", "ant")).to eq(true)
+  end
+
+  it "tests that 'Tom Riddle' and 'Voldemort' return true" do
+    word_checker = AnagramChecker.new
+    expect(word_checker.check_if_anagram("fish", "dog")).to eq(false)
+  end
+
 
 end
