bd47fdeebd09ebbe6afb1d77033512055191dd45
katerina-codes <katerinaldg@gmail.com>
2017-03-23 19:34:31 +0000

    Second test to test if two words are anagrams is green.

--- ex24_anagram_checker_challenge/spec/anagram_checker_spec.rb
+++ ex24_anagram_checker_challenge/spec/anagram_checker_spec.rb
@@ -3,8 +3,13 @@ require 'anagram_checker'
 describe AnagramChecker do
 
   it "tests that words 'ab, ba' return true" do
-  check_word = AnagramChecker.new
-  expect(check_word.check_if_anagram("ab", "ba")).to eq(true)
+    check_word = AnagramChecker.new
+    expect(check_word.check_if_anagram("ab", "ba")).to eq(true)
+  end
+
+  it "tests that words 'ca, ac' return true" do
+    check_word = AnagramChecker.new
+    expect(check_word.check_if_anagram("ac", "ca")).to eq(true)
   end
 
 end
