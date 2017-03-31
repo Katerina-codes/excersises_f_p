681ca0f4260aed26dad6b75b66688ca4c1a9b5df
katerina-codes <katerinaldg@gmail.com>
2017-03-31 14:55:08 +0100

    Rafactored code with formula to calculate area.

--- ex9_Paint_Calculator/lib/paint_calculator.rb
+++ ex9_Paint_Calculator/lib/paint_calculator.rb
@@ -1,13 +1,6 @@
 class PaintCalculator
 
   def get_ceiling_area(length, width)
-    if width == 0
-      0
-    elsif width == 1
-      1
-    else
-      2
-    end
-  end
-
+    length * width
+  end 
 end
