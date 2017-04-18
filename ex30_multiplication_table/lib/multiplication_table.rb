9f4c291884925bd94dbe7698cf66cce982fb665e
Katerina <katerinaldg@gmail.com>
2017-04-18 11:51:27 +0100

    Refactored code to make it shorter

--- ex30_multiplication_table/lib/multiplication_table.rb
+++ ex30_multiplication_table/lib/multiplication_table.rb
@@ -1,13 +1,7 @@
 class MultiplicationTable
 
 	def multiply(number_to_multiply, multiply_number_by)
-		if multiply_number_by == 0
-			0
-		elsif multiply_number_by == 1
-			1
-		else
-			2
-		end
+		number_to_multiply * multiply_number_by
 	end
 
 end
